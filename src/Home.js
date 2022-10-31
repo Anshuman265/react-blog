f270cb0f1d87ce8ca350487c84934af9896a3669
Anshuman  Verma <72643139+Anshuman265@users.noreply.github.com>
2022-10-31 23:17:12 +0530

    useState hook added

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
-    const handleClick = () => {
-        console.log('hello,ninjas');
-    }
+    const [name,setName] = useState('mario');
+    const [age,setAge] = useState(25);
     const handleClickAgain = (name , e) => {
-        console.log('hello ' + name , e.target);
+        setName('luigi');
+        setAge(35);
     }
+// Changing the state value triggers react to re-render the state-component
 // We automatically get access to the event parameter
     return ( 
         <div className="home">
             <h2> Research Blog </h2>
-            <button onClick={handleClick}>Click me</button>
+            {/*<button onClick={handleClick}>Click me</button> */}
+            <p> {name } is {age}</p>
             <button onClick={(e) => handleClickAgain('Zelda' , e )}>Click me again</button>
         </div>
      );
