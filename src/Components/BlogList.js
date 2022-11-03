6b7aa791f148075505bdf99ba762ad6bfb3fec60
Anshuman  Verma <72643139+Anshuman265@users.noreply.github.com>
2022-11-03 16:44:04 +0530

    handleDelete function added

--- src/Components/BlogList.js
+++ src/Components/BlogList.js
@@ -5,11 +5,12 @@ const BlogList = (props) => {
             {props.blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
+                    <p>{blog.body}</p>
                     <p>Written by:- {blog.author}</p>
+                    <button onClick={() => props.handleDelete(blog.id)}> Delete Blog </button>
                 </div>
             ))}
         </div>
-
     );
 }
  
--- src/Home.js
+++ src/Home.js
@@ -11,10 +11,17 @@ const Home = () => {
         }
     */ }
     const [blogs,setBlogs] = useState([ 
-        {'title':'Blog number 1', body:'Body of blog 1' , author:'Author 1', id:1},
-        {'title':'Blog number 2', body:'Body of blog 2' , author:'Author 2', id:2},
-        {'title':'Blog number 3', body:'Body of blog 3' , author:'Author 3', id:3},
+        {'title':'Blog number 1', body:'Body of blog 1' , author:'Devashish', id:1},
+        {'title':'Blog number 2', body:'Body of blog 2' , author:'Darshan', id:2},
+        {'title':'I am vegeance', body:'I am the night' , author:'Batman', id:3},
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => {
+            return (blog.id !== id);
+        });
+        setBlogs(newBlogs);
+    }
 // Changing the state value triggers react to re-render the state-component
 // We automatically get access to the event parameter
     return ( 
@@ -22,7 +29,7 @@ const Home = () => {
             {/*<button onClick={handleClick}>Click me</button> 
             <p> {name } is {age}</p>
     <button onClick={(e) => handleClickAgain('Zelda' , e )}>Click me again</button> */}
-            <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete = {handleDelete}/>
         </div>
      );
 }
