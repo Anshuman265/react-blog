17121a09032b250933d7ce116ebefb10b58be1a9
Anshuman  Verma <72643139+Anshuman265@users.noreply.github.com>
2022-11-28 00:57:40 +0530

    Added Loading component

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,8 @@ import { useState,useEffect } from "react";
 import BlogList from "./Components/BlogList";
 
 const Home = () => {
-    const [blogs,setBlogs] = useState([ 
-        {'title':'Blog number 1', body:'Body of blog 1' , author:'Devashish', id:1},
-        {'title':'Blog number 2', body:'Body of blog 2' , author:'Darshan', id:2},
-        {'title':'I am vegeance', body:'I am the night' , author:'Batman', id:3},
-    ]);
+    const [blogs,setBlogs] = useState(null);
+    const [isPending,setIsPending] = useState(true);
     // command to run json-server = npx json-server --watch data/db.json --port 8000
     const [name,setName] = useState('mario');
     useEffect(() => {
@@ -16,12 +13,14 @@ const Home = () => {
             }
         ).then(data=>{
             setBlogs(data);
+            setIsPending(false);
         })
     }, []);
 // Changing the state value triggers react to re-render the state-component
 // We automatically get access to the event parameter
     return ( 
         <div className="home">
+            {isPending && <div> Loading... </div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
      );
